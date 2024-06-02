dd67a71e3c4c438ef727785c2ea2017b2fdaae42
Dohyeon Kim <xingchen8837@gmail.com>
2024-06-03 03:45:42 +0900

    주석 제거

--- DRAM.h
+++ DRAM.h
@@ -4,69 +4,84 @@
 #include <freertos/task.h>
 #include <freertos/semphr.h>
 
-// Mutex variable declaration
-// portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 SemaphoreHandle_t io_mutex;
 
-namespace ENV {
+namespace ENV
+{
     const int WL_PIN = 18;
 };
 
-class half_byte {
+class half_byte
+{
 public:
     int num;
 
-    half_byte(int num) {
-        if (num >= pow(2, 4)) {
+    half_byte(int num)
+    {
+        if (num >= pow(2, 4))
+        {
             this->num = pow(2, 4) - 1; // Corrected value to be within 4 bits
         }
-        else {
+        else
+        {
             this->num = num;
         }
     }
 
-    void set(int num) {
-        if (num >= pow(2, 4)) {
+    void set(int num)
+    {
+        if (num >= pow(2, 4))
+        {
             this->num = pow(2, 4) - 1; // Corrected value to be within 4 bits
         }
-        else {
+        else
+        {
             this->num = num;
         }
     }
 };
 
-class Byte {
-    private:
-        int* V;
-    public:
-        Byte(int p1, int p2, int p3, int p4) {
-            V = new int[4]{p1, p2, p3, p4};
+class Byte
+{
+private:
+    int *V;
+
+public:
+    Byte(int p1, int p2, int p3, int p4)
+    {
+        V = new int[4]{p1, p2, p3, p4};
+    }
+
+    half_byte read()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            pinMode(V[i], INPUT);
+        }
+        digitalWrite(ENV::WL_PIN, HIGH);
+        int t = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            t += digitalRead(V[i]) * (int)pow(2, i);
+        }
+        return half_byte(t);
+    }
+
+    void write(bool isTask, half_byte hb)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            pinMode(V[i], OUTPUT);
+        }
+        digitalWrite(ENV::WL_PIN, LOW);
+
+        for (int i = 0; i < 4; i++)
+        {
+            digitalWrite(V[i], (hb.num & (int)pow(2, i)) >> i);
         }
 
-        half_byte read() {
-            for (int i = 0; i < 4; i++) {
-                pinMode(V[i], INPUT);
-            }
-            digitalWrite(ENV::WL_PIN, HIGH);
-            int t = 0;
-            for (int i = 0; i < 4; i++) {
-                t += digitalRead(V[i]) * (int)pow(2, i);
-            }
-            return half_byte(t);
-        }
-
-        void write(bool isTask, half_byte& hb) {
-            for (int i = 0; i < 4; i++) {
-                pinMode(V[i], OUTPUT); 
-            }
-            digitalWrite(ENV::WL_PIN, LOW);
-            
-            for (int i = 0; i < 4; i++) {
-                digitalWrite(V[i], (hb.num & (int)pow(2, i)) >> i);
-            }
-            
-            digitalWrite(ENV::WL_PIN, HIGH);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            digitalWrite(ENV::WL_PIN, LOW);
-        }
+        digitalWrite(ENV::WL_PIN, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(100));
+        digitalWrite(ENV::WL_PIN, LOW);
+    }
 };
--- DRAM.ino
+++ DRAM.ino
@@ -9,16 +9,14 @@ void Refresh(void *pvParams);
 
 Byte B = Byte(27, 14, 12, 13);
 
-void setup() {
-    // pinMode(27, INPUT);
-    // pinMode(14, INPUT);
-    // pinMode(12, INPUT);
-    // pinMode(13, INPUT);
+void setup()
+{
     pinMode(ENV::WL_PIN, OUTPUT);
     Serial.begin(115200);
 
     io_mutex = xSemaphoreCreateMutex();
-    if (io_mutex == NULL) {
+    if (io_mutex == NULL)
+    {
         Serial.println("Mutex error");
     }
 
@@ -29,50 +27,54 @@ void setup() {
         4096,
         NULL,
         1,
-        &Refresher
-    );
+        &Refresher);
+
+    B.write(false, half_byte(0));
 }
 
-void loop() {
-    if (Serial.available()) {
+void loop()
+{
+    if (Serial.available())
+    {
         String input = Serial.readStringUntil('\n');
-        input.trim(); // Remove leading and trailing whitespace
-
+        input.trim();
         char command = input[0];
 
-        if (command == 'r') {
+        if (command == 'r')
+        {
             Serial.printf("읽은 값: %d\n", B.read().num);
-        } else if (command == 'c') {
+        }
+        else if (command == 'c')
+        {
             input.remove(0, 2);
             int data = input.toInt();
 
             half_byte t = half_byte(data);
 
-            // portENTER_CRITICAL(&mux);
             xSemaphoreTake(io_mutex, portMAX_DELAY);
             B.write(false, t);
             xSemaphoreGive(io_mutex);
-            // portEXIT_CRITICAL(&mux);
 
-            Serial.println("쓰기 완료.");
-        } else {
+            Serial.printf("값 %d 쓰기 완료.\n", t.num);
+        }
+        else
+        {
             Serial.println("Invalid command");
         }
     }
 }
 
-void Refresh(void *pvParams) {
-    while (true) {
-        // portENTER_CRITICAL(&mux); // Start critical section
-
+void Refresh(void *pvParams)
+{
+    while (true)
+    {
         xSemaphoreTake(io_mutex, portMAX_DELAY);
 
         half_byte t = B.read();
         B.write(true, t);
-        // portEXIT_CRITICAL(&mux); // End critical section
-        
+
         xSemaphoreGive(io_mutex);
 
-        vTaskDelay(pdMS_TO_TICKS(100)); // Use FreeRTOS delay function
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
